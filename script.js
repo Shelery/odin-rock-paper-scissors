f7dff002f2a9d91d8a94207ba5bb600af6440210
Shelery <eszter.with.a.z@gmail.com>
2022-07-05 23:19:41 +0200

    Display roundResult in a div

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 // Get buttons
 const buttons = document.querySelectorAll(`button`);
 
+const message = document.querySelector('#message')
 let computerSelection;
 let playerSelection;
 
 // Initialize vars to store scores
-let computerPoints = 0;
-let playerPoints = 0;
+var computerPoints = 0;
+var playerPoints = 0;
 
 const roundResult = playRound;
-buttons.forEach(button => button.addEventListener('click', roundResult));
+for (let i = 0; i < 5; i++){
+    buttons.forEach(button => button.addEventListener('click', roundResult));
+}
+
 
 
 // Calculate winner
 resultMessage = `And the result is...`
-
-console.log(getResultMessage(computerSelection, playerSelection))
+// NOTE : computerSelection and playerSelection are not returned (yet)
+// BUT it should be updated yet it is not and round limit is not working either ^.^"
+console.log(getResultMessage(computerPoints, playerPoints))
 
 
 // Functions
+// Play one round of R-P-S and save points
+// get input function calls inside the function
+function playRound(e) {
+    const computerSelection = computerPlay();
+    const playerSelection = e.target.id;
+    // Don't add any points if they chose the same
+    if (computerSelection === playerSelection) {
+        return message.textContent =`Draw! You both chose ${computerSelection}!`
+    } else {
+        // List every case where the computer win, add points to computer
+        if ((computerSelection === `rock` && playerSelection === `scissors`)
+            || (computerSelection === `paper` && playerSelection === `rock`)
+            || (computerSelection === `scissors` && playerSelection === `paper`)) {
+            computerPoints++;
+            if (computerSelection === `scissors`) {
+                return message.textContent =`You Lose! ${toCapital(computerSelection)} beat ${playerSelection}!`
+            }
+            return message.textContent =`You Lose! ${toCapital(computerSelection)} beats ${playerSelection}!`
+        } else {
+            // Cases when Player win
+            playerPoints++;
+            if (playerSelection === `scissors`) {
+                return message.textContent =`You Win! ${toCapital(playerSelection)} beat ${computerSelection}!`
+            }
+            return message.textContent =`You Win! ${toCapital(playerSelection)} beats ${computerSelection}!`
+        }
+    }
+
+}
+
 // Get random choice from computer
 function computerPlay() {
     n = Math.floor(Math.random() * 3);
@@ -32,36 +67,6 @@ function computerPlay() {
     }
 }
 
-
-// Play one round of R-P-S and save points
-// get input function calls inside the function
-function playRound(e) {
-    const computerSelection = computerPlay();
-    const playerSelection = e.target.id;
-    // Don't add any points if they chose the same
-    if (computerSelection === playerSelection) {
-        return console.log(`Draw! You both chose ${computerSelection}!`)
-    } else {
-        // List every case where the computer win, add points to computer
-        if ((computerSelection === `rock` && playerSelection === `scissors`)
-            || (computerSelection === `paper` && playerSelection === `rock`)
-            || (computerSelection === `scissors` && playerSelection === `paper`)) {
-            computerPoints++;
-            if (computerSelection === `scissors`) {
-                return console.log(`You Lose! ${toCapital(computerSelection)} beat ${playerSelection}!`)
-            }
-            return console.log(`You Lose! ${toCapital(computerSelection)} beats ${playerSelection}!`)
-        } else {
-            // Cases when Player win
-            playerPoints++;
-            if (playerSelection === `scissors`) {
-                return console.log(`You Win! ${toCapital(playerSelection)} beat ${computerSelection}!`)
-            }
-            return console.log(`You Win! ${toCapital(playerSelection)} beats ${computerSelection}!`)
-        }
-    }
-
-}
 // Capitalize first letter
 function toCapital(string) {
     if (typeof string !== "string") return;
